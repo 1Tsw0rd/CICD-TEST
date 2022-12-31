3e3e6a36e5df0567aa12cb2c75653b9c0f199b62
SeDevel <ohhojin25@naver.com>
2023-01-01 00:20:59 +0900

    Update TestContorller.java

--- cicdtest/src/main/java/com/sparta/cicdtest/controller/TestContorller.java
+++ cicdtest/src/main/java/com/sparta/cicdtest/controller/TestContorller.java
@@ -8,6 +8,6 @@ public class TestContorller {
 
     @GetMapping("/")
     public String getTest() {
-        return "CI/CD 테스트 중입니다. 제발 되라 ㅠㅠㅠddddd";
+        return "vvvCI/CD 테스트 중입니다. 제발 되라 ㅠㅠㅠddddd";
     }
 }
