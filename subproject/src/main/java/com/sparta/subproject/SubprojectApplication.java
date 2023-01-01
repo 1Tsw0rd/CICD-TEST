d801b1a29016735402c4a0c8ed72b637e36874d7
SeDevel <ohhojin25@naver.com>
2023-01-01 21:11:53 +0900

    init subproject

---
+++ subproject/src/main/java/com/sparta/subproject/SubprojectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.subproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubprojectApplication.class, args);
+	}
+
+}
